a412b91dfa4c1db26e93e9e46e88d93ab21aeff6
aakashkhatter1296 <khatteraakash1996@gmail.com>
2018-03-08 14:38:01 +0530

    added component SearchBar

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import '../style/App.css';
+import SearchBar from "./search_bar";
+import YTSearch from "youtube-api-search";
+
+const API_KEY = "AIzaSyA33ekq2jSbMF7fRSyzBKIpKMohPS2oYfM";
 
 class App extends Component {
+  constructor(props){
+  	super(props);
+
+  	this.state = {videos: []};
+
+  	YTSearch({key: API_KEY, term: "free running"}, (videos) => {
+		this.setState({ videos });
+		console.log(this.state.videos);
+	});
+  }
+
   render() {
     return (
       <div className="App">
-        <h2>Hello React</h2>
+        <SearchBar />
       </div>
     );
   }
