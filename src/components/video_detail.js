865ab3b24ffeeedd57c772a77cbcb01a29ec026d
aakashkhatter1296 <khatteraakash1996@gmail.com>
2018-03-26 13:56:57 +0530

    fixed warnings

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -12,7 +12,7 @@ export default function VideoDetail({video}) {
 	return(
 		<div className="video-detail col-md-8">
 			<div className="embed-responsive embed-responsive-16by9">
-				<iframe src={url} className="embed-responsive-item"></iframe>
+				<iframe title="Youtube Video" src={url} className="embed-responsive-item"></iframe>
 			</div>
 			<div className="details">
 				<div><h3>{ video.snippet.title }</h3></div>
